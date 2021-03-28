bfcbef3b9aff215e064116696ed1edb261a08807
Charles L <charles.lobo@gmail.com>
2021-03-29 09:12:34 +1100

    refactor: minor cleanup

--- makeEnglish.js
+++ makeEnglish.js
@@ -3,28 +3,26 @@ const request = require("request")
 
 // This function will convert the incoming non-english from avatar user to english text and return the same.
 function makeEnglish(msg, callback){
-
     let translate_options = {
-          url: 'http://149.202.214.34:5000/toenglish?',
-          form: {
+        url: 'http://149.202.214.34:5000/toenglish?',
+        form: {
             sentence: msg,
-          }
         }
-      request.post(translate_options, function(err, res){
-          if(err) callback(err)
-          else {
+    }
+    request.post(translate_options, function(err, res){
+        if(err) callback(err)
+        else {
             try {
-              
-              let translated = JSON.parse(res.body)
-              let english_msg = translated.Output
-              let lang = translated.lang
-              callback(null, lang, english_msg)
+                let translated = JSON.parse(res.body)
+                let english_msg = translated.Output
+                let lang = translated.lang
+                callback(null, lang, english_msg)
             } catch(e) {
-              callback(e)
+                callback(e)
             }
         }
     })
-  }
+}
 
 
 module.exports = makeEnglish
--- makeOriginal.js
+++ makeOriginal.js
@@ -4,24 +4,24 @@ const request = require("request")
 // This function will convert the english response from elife-ai to the original language that the avatar user had given.
 function makeOriginal(lang, msg, callback) {
     let translate_options = {
-      url: 'http://149.202.214.34:5000/tooriginal?',
-      form: {
-        sentence: msg,
-        toLang : lang
-      }}
+        url: 'http://149.202.214.34:5000/tooriginal?',
+        form: {
+            sentence: msg,
+            toLang : lang
+        }}
     request.post(translate_options, function(error,result) {
-      if(error || (result.statusCode == 404)){
-        callback(error, msg)
-      } else {
-          try {
-            let translated = JSON.parse(result.body)
-            let translated_msg = translated.Output
-            callback(null,translated_msg)
-          } catch(e) {
-            callback(e, msg)
-          }
-      }
+        if(error || (result.statusCode == 404)){
+            callback(error, msg)
+        } else {
+            try {
+                let translated = JSON.parse(result.body)
+                let translated_msg = translated.Output
+                callback(null,translated_msg)
+            } catch(e) {
+                callback(e, msg)
+            }
+        }
     })
-  }   
+}
 
 module.exports = makeOriginal
